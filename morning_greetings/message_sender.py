6d743f2b25bdb3d11f7aa11620268122596d2f47
vasstha <157070114+vasstha@users.noreply.github.com>
2024-10-15 23:32:37 +0200

    new files

--- morning_greetings/message_sender.py
+++
@@ -1,7 +0,0 @@
-
-def send_message(name, email, message):
-    if not email:
-        raise ValueError("Email Address is missing.")
-    
-    print(f"Sending message to {name} to this email address {email}: {message}.")
-    return True
